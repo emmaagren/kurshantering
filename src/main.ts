f41a22e7ace8b2827ff0a2d705a7f1f1c6f0d269
Emma Ågren <agren.gastronomi@hotmail.com>
2026-06-02 12:11:22 +0200

    Lägger till localStorage och funktion för att visa kurser.

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,53 @@ import './style.css'
 import { CourseInfo } from './interfaces'
 
 let courses: CourseInfo[] = [];
+
+//LocalStorage
+
+function loadCourses(): void {
+
+  const storedCourses: string | null =
+    localStorage.getItem("courses");
+
+  if (storedCourses) {
+    courses = JSON.parse(storedCourses);
+  }
+}
+
+function saveCourses(): void {
+  localStorage.setItem(
+    "courses",
+    JSON.stringify(courses)
+  );
+}
+
+
+//Visa kurser
+
+function renderCourses(): void {
+
+  const courseList =
+    document.getElementById("courseList") as HTMLUListElement;
+
+  courseList.innerHTML = "";
+
+  courses.forEach((course: CourseInfo) => {
+
+    const li: HTMLLIElement =
+      document.createElement("li");
+
+    li.innerHTML = `
+    <strong>${course.code}</strong
+    - ${course.name}
+    (${course.progression})
+    <a href="${course.syllabus}" target="_blank">
+    Kursplan
+    </a>
+    button data-code="${course.code}">
+    Radera
+    </button>
+    `;
+
+    courseList.appendChild(li);
+  });
+}
